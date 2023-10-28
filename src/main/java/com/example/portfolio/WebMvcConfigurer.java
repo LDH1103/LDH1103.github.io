2a2abd97a2454856a9c40128a6e3ac22281e1b80
LDH1103 <ldh517525@gmail.com>
2023-10-28 16:21:30 +0900

    portfolio

--- src/main/java/com/example/portfolio/WebMvcConfigurer.java
+++
@@ -1,8 +0,0 @@
-package com.example.portfolio;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
-public abstract class WebMvcConfigurer {
-    public abstract void addResourceHandlers(ResourceHandlerRegistry registry);
-}
